a97b75d4af74e171748c3a5dd23db9ce9d7fb529
Yahya Ganjo <yahya.ganjo@gmail.com>
2021-10-03 23:30:03 +0200

    create login controller + model

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,31 @@
+import bcrypt from "bcryptjs";
+import User from "../models/user.js";
+
 const login = async (req, res) => {
+  const { email, password } = req.body;
   try {
-    res.status(200).json({ success: true });
-  } catch (error) {}
+    const existedUser = await User.findOne({ email });
+    if (!existedUser) {
+      return res
+        .status(404)
+        .json({ success: false, msg: "Account doesn't exist" });
+    }
+    const correctPassword = bcrypt.compare(password, existedUser.password);
+    if (!correctPassword) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "Invalid credentials" });
+    }
+
+    const { _id, first_name, last_name, email } = existedUser;
+    const phone = existedUser?.phone ? existedUser.phone : "";
+    res.status(200).json({ success: true, result: existedUser });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, msg: "Something went wrong, try later please" });
+    console.log(error);
+  }
 };
 
 export { login };
---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.SChema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  phone: String,
+});
+
+const User = mongoose.Model("users", userSchema);
+export default User;
