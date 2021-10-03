5cdf1351b7a7191448801fe4f09d16be5700f88e
Mohammad <malnajjar16@gmail.com>
2021-10-03 23:45:25 +0200

    payment 2de fase

--- server/controllers/payment.js
+++ server/controllers/payment.js
@@ -1,7 +1,39 @@
-// const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
+import stripe from 'stripe';
+const payStripe = stripe(process.env.STRIPE_PRIVATE_KEY);
 
 const paymentHandle = async (req, res) => {
-  res.send('payment');
+  const tickets = req.body;
+
+  const purchasedTickets = tickets.map((ticket) => {
+    return {
+      price_data: {
+        currency: 'EUR',
+        product_data: {
+          name: ticket.typeName,
+        },
+        unit_amount: ticket.price * 100,
+      },
+      quantity: ticket.quantity,
+    };
+  });
+  try {
+    const session = await payStripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+      line_items: purchasedTickets,
+      success_url: `http://localhost:3001`,
+      cancel_url: `http://localhost:3001`,
+    });
+    res.json({ url: session.url });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: {
+        msg: 'payment failed',
+        success: false,
+      },
+    });
+  }
 };
 
 export default paymentHandle;
--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const startServer = async () => {
   try {
     await connectDB();
