6cdee61a1bc00bd18ec04da46c7064a05ccf8665
Yahya Ganjo <yahya.ganjo@gmail.com>
2021-10-07 00:14:26 +0200

    binding register component to the navdrawer

--- client/src/components/nav/NavDrawer.js
+++ client/src/components/nav/NavDrawer.js
@@ -1,26 +1,37 @@
-import React from "react";
-import { Grid, List } from "@mui/material";
+import { useState } from 'react';
+import { Grid, List } from '@mui/material';
 
-import { Box } from "@mui/system";
-import { useStyles } from "./NavStyles";
-import DrawerListButton from "./DrawerListButton";
+import { Box } from '@mui/system';
+import { useStyles } from './NavStyles';
+import Register from '../user/register/Register';
+import DrawerListButton from './DrawerListButton';
 
 export const NavDrawer = ({ drawer }) => {
+  const [isRegister, setIsRegister] = useState(false);
+
   const classes = useStyles({ drawer });
   const list = () => (
-    <Box className={classes.drawerContainer}>
-      <List>
-        <Grid container className={classes.drawerDropDownOuter}>
-          <Grid container className={classes.drawerDropDown}>
-            <DrawerListButton text="login" />
-            <DrawerListButton text="program" />
-            <DrawerListButton text="tickets" />
-            <DrawerListButton text="about" />
-            <DrawerListButton text="contact" />
+    <>
+      <Box className={classes.drawerContainer}>
+        <List>
+          <Grid container className={classes.drawerDropDownOuter}>
+            <Grid container className={classes.drawerDropDown}>
+              <div onClick={() => setIsRegister(true)}>
+                <DrawerListButton text='login' />
+              </div>
+              <DrawerListButton text='program' />
+              <DrawerListButton text='tickets' />
+              <DrawerListButton text='about' />
+              <DrawerListButton
+                onClick={() => setIsRegister(true)}
+                text='contact'
+              />
+            </Grid>
           </Grid>
-        </Grid>
-      </List>
-    </Box>
+        </List>
+      </Box>
+      {isRegister && <Register setIsRegister={setIsRegister} />}
+    </>
   );
 
   return <>{list()}</>;
