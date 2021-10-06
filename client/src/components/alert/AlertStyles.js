247b31169bdcf7ed0c9990e16dd0a2b58d6dc089
mohammedagl6 <mohammedagl6@gmail.com>
2021-10-06 23:45:48 +0200

    Update AlertStyles.js

--- client/src/components/alert/AlertStyles.js
+++ client/src/components/alert/AlertStyles.js
@@ -1,22 +1,30 @@
-import styled from 'styled-components';
+import { makeStyles } from '@mui/styles';
 
-export const AlertContainer = styled.div`
-  max-width: 1170px;
-  position: relative;
-  padding: 0.75rem 1.25rem;
-  margin-bottom: 1rem;
-  border: 1px solid transparent;
-  border-radius: 0.25rem;
-  margin: 1rem auto;
-  text-align: center;
-  color: ${(props) => props.color};
-  background-color: ${(props) => props.background};
-  border-color: ${(props) => props.borderColor};
-`;
-
-export const AlertCancel = styled.div`
-  position: absolute;
-  right: 0;
-  top: 0;
-  cursor: pointer;
-`;
+export const useStyles = makeStyles({
+  alert: {
+    maxWidth: '1170px',
+    position: 'relative',
+    padding: '0.75rem 1.25rem',
+    marginBottom: '1rem',
+    border: '1px solid transparent',
+    borderRadius: '0.25rem',
+    margin: '1rem auto',
+    textAlign: 'center',
+  },
+  success: {
+    color: '#155724',
+    backgroundColor: '#d4edda',
+    borderColor: '#c3e6cb',
+  },
+  danger: {
+    color: '#721c24',
+    backgroundColor: '#f8d7da',
+    borderColor: '#f5c6cb',
+  },
+  alertCancel: {
+    position: 'absolute',
+    right: '0',
+    top: '0',
+    cursor: 'pointer',
+  },
+});
