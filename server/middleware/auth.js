fb48aa3978ceccc9016d371f0fc775f8e55841bc
Ameri <mohammedagl6@gmail.com>
2021-10-07 10:46:06 +0200

    removed checking google token

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,17 +3,8 @@ import jwt from 'jsonwebtoken';
 const auth = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const isCustom = token?.length < 500;
-
-    let decodedData;
-
-    if (isCustom) {
-      decodedData = jwt.verify(token, process.env.JWT_SECRET);
-      req.userId = decodedData?.id;
-    } else {
-      decodedData = jwt.decode(token);
-      req.userId = decodedData?.sub;
-    }
+    const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decodedData.id;
     next();
   } catch (error) {
     res
