d87e7a6eaca7f104772f922618b4b5f0eea88cf9
Ameri <mohammedagl6@gmail.com>
2021-10-09 11:37:57 +0200

    fixing the requested rout

--- client/src/actions/profileAction.js
+++ client/src/actions/profileAction.js
@@ -2,7 +2,7 @@ const url = process.env.REACT_APP_SERVER_URL;
 
 export const getOrders = async (user) => {
   try {
-    const response = await fetch(url + '/order/getOrders', {
+    const response = await fetch(url + '/order', {
       headers: { authorization: `Bearer ${user?.token}` },
     });
     const data = response.json();
