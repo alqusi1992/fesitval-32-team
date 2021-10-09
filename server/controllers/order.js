5299ddd54c8a34bde76ca95e21743feb2e1336cb
Mohammad <malnajjar16@gmail.com>
2021-10-09 11:53:02 +0200

    fix status code

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -11,6 +11,8 @@ export const createOrder = async (req, res) => {
     });
     if (order.id) {
       res.status(200).json({ success: true, order });
+    } else {
+      res.status(400).json({ success: false, msg: 'Order cannot be created' });
     }
   } catch (error) {
     res.status(500).json({ success: false, msg: error });
