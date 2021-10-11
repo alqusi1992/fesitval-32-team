83265446d29277733d7112f5e47d71a07714a02f
Mohammad <malnajjar16@gmail.com>
2021-10-11 15:37:32 +0200

    fix check failed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
           </Container>
           <Footer />
         </Router>
-        {/* <PayButton /> */}
+        <PayButton />
       </ThemeProvider>
     </GuestProvider>
   );
