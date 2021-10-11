c85b525cf7d92afada27f1c99735fd3ab1e404a0
Hani al Sabti <h.sabti87@gmail.com>
2021-10-11 22:31:07 +0200

    added auth to delete and update

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import {
-  login, register, deleteAccount, updateAccount,
+  login,
+  register,
+  deleteAccount,
+  updateAccount,
 } from '../controllers/user.js';
+import auth from '../middleware/auth.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/login', login);
 userRouter.post('/register', register);
-userRouter.delete('/delete/:id', deleteAccount);
-userRouter.patch('/update/:id', updateAccount);
+userRouter.delete('/delete', auth, deleteAccount);
+userRouter.patch('/update', auth, updateAccount);
 
 export default userRouter;
