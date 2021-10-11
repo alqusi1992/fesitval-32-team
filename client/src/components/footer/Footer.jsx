b5f8d2738806c9e456b78db69c97bca00e0b8102
Mohammad <malnajjar16@gmail.com>
2021-10-11 13:33:45 +0200

    socialMedia icons styled

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,20 +1,23 @@
-import { Grid } from '@mui/material'
-import React from 'react'
-import {FooterWrapper} from './FooterStyles'
-import FooterList from './FooterList'
-import SocialMediaIcons from './SocialMediaIcons'
+import { Grid } from '@mui/material';
+import React from 'react';
+import { FooterWrapper } from './FooterStyles';
+import FooterList from './FooterList';
+import SocialMediaIcons from './SocialMediaIcons';
 
 const Footer = () => {
-    
-    return (
-        <FooterWrapper>
-            <Grid container alignItems='space-between' height='100%'>
-                <Grid item md={6} xs={12}></Grid>
-                <Grid item md={3} xs={12}><FooterList/></Grid>
-                <Grid item md={3} xs={12}><SocialMediaIcons/></Grid>
-            </Grid>
-        </FooterWrapper>
-    )
-}
+  return (
+    <FooterWrapper>
+      <Grid container alignItems='space-between' height='100%'>
+        <Grid item md={6} xs={12}></Grid>
+        <Grid item md={3} xs={12}>
+          <FooterList />
+        </Grid>
+        <Grid item md={1} xs={12}>
+          <SocialMediaIcons />
+        </Grid>
+      </Grid>
+    </FooterWrapper>
+  );
+};
 
-export default Footer
+export default Footer;
--- client/src/components/footer/SocialMediaIcons.jsx
+++ client/src/components/footer/SocialMediaIcons.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import {FacebookIcon, InstagramIcon, YouTubeIcon} from '@mui/icons-material';
+import React from 'react';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 import { Grid } from '@mui/material';
 
-
 const SocialMediaIcons = () => {
-    return (
-        <Grid container >
-            <Grid item xm={2}><FacebookIcon/></Grid>
-            <Grid item xm={2}><InstagramIcon/></Grid>
-            <Grid item xm={2}><YouTubeIcon/></Grid>
-            
-        </Grid>
-    )
-}
+  return (
+    <Grid container justifyContent='space-between' sx={{ paddingTop: '20px' }}>
+      <Grid item xm={2}>
+        <FacebookIcon sx={{ color: '#1877F2', fontSize: '40px' }} />
+      </Grid>
+      <Grid item xm={2}>
+        <InstagramIcon sx={{ color: '#E4405F', fontSize: '40px' }} />
+      </Grid>
+      <Grid item xm={2}>
+        <YouTubeIcon sx={{ color: '#CD201F', fontSize: '40px' }} />
+      </Grid>
+    </Grid>
+  );
+};
 
-export default SocialMediaIcons
+export default SocialMediaIcons;
