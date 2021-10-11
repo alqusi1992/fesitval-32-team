0deaf6873dd7c1ac1b266a743332876e4a85a380
“Mohammad <malqusi@yahoo.com>
2021-10-11 17:09:37 +0200

    created delete and update controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -7,18 +7,11 @@ export const login = async (req, res) => {
   try {
     const existedUser = await User.findOne({ email });
     if (!existedUser) {
-      return res
-        .status(404)
-        .json({ success: false, msg: "Account doesn't exist" });
+      return res.status(404).json({ success: false, msg: "Account doesn't exist" });
     }
-    const correctPassword = await bcrypt.compare(
-      password,
-      existedUser.password,
-    );
+    const correctPassword = await bcrypt.compare(password, existedUser.password);
     if (!correctPassword) {
-      return res
-        .status(400)
-        .json({ success: false, msg: 'Invalid credentials' });
+      return res.status(400).json({ success: false, msg: 'Invalid credentials' });
     }
     const { _id, firstName, lastName } = existedUser;
     const phone = existedUser?.phone ? existedUser.phone : '';
@@ -43,15 +36,11 @@ export const login = async (req, res) => {
 };
 
 export const register = async (req, res) => {
-  const {
-    email, password, firstName, lastName, phone,
-  } = req.body;
+  const { email, password, firstName, lastName, phone } = req.body;
   try {
     const existedUser = await User.findOne({ email });
     if (existedUser) {
-      return res
-        .status(400)
-        .json({ success: false, msg: 'Account already exists' });
+      return res.status(400).json({ success: false, msg: 'Account already exists' });
     }
     const hashedPassword = await bcrypt.hash(password, 12);
     const result = await User.create({
@@ -82,5 +71,42 @@ export const register = async (req, res) => {
       success: false,
       msg: 'Something went wrong, try again later please',
     });
+  }
+};
+
+export const deleteAccount = async (req, res) => {
+  try {
+    const existedUser = await User.findById(req.params.id);
+    const { _id } = existedUser;
+    if (!existedUser) {
+      return res.status(404).json({ success: false, msg: 'User not found' });
+    }
+    await User.findByIdAndDelete(_id);
+    return res.json({ success: true, msg: 'user is deleted' });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({ success: false, msg: 'User not found' });
+  }
+};
+
+export const updateAccount = async (req, res) => {
+  const { email, password, firstName, lastName, phone } = req.body;
+  const useObj = {
+    firstName,
+    lastName,
+    email,
+    password,
+    phone,
+  };
+
+  try {
+    const user = await User.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $set: useObj },
+      { upsert: true, new: true },
+    );
+    return res.status(200).json({ success: true, msg: 'Success', user });
+  } catch (error) {
+    return res.status(500).json({ success: false, msg: 'Server Error' });
   }
 };
