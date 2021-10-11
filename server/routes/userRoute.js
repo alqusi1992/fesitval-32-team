79bb89963e31ea577661981feda73b452b5c572d
“Mohammad <malqusi@yahoo.com>
2021-10-11 17:10:03 +0200

    created delete and update route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { login, register } from '../controllers/user.js';
+import { login, register, deleteAccount, updateAccount } from '../controllers/user.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/login', login);
 userRouter.post('/register', register);
+userRouter.delete('/delete/:id', deleteAccount);
+userRouter.patch('/update/:id', updateAccount);
+
 export default userRouter;
