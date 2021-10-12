f0b559a4c7e91e81381cf4b21e99ca214b679d98
Ameri <mohammedagl6@gmail.com>
2021-10-12 22:01:29 +0200

    applying eslint

--- client/src/components/loading/Loading.js
+++ client/src/components/loading/Loading.js
@@ -1,7 +1,5 @@
-import * as React from 'react';
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
-import Button from '@mui/material/Button';
 import { useValue } from '../../context/globalContext';
 
 export default function SimpleBackdrop() {
