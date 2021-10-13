1343e2365ed6a97a7a1f5f43f3fcdae8d1abfd9c
Mouawya <mouawya88@gmail.com>
2021-10-13 11:23:07 +0200

    form validation fase-1

--- client/src/components/orderStepper/guestForm/GuestForm.js
+++ client/src/components/orderStepper/guestForm/GuestForm.js
@@ -1,5 +1,5 @@
-import React from 'react';
-// import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import {
   FormWrapper,
   ButtonWrapper,
@@ -15,22 +15,29 @@ import IconButton from '@mui/material/IconButton';
 import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 const GuestForm = () => {
-  // const {
-  //   handleSubmit,
-  //   register,
-  //   formState: { errors },
-  // } = useForm();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
   const { guestUserOrder, setGuestUserOrder } = useGuestContext();
 
-  const [values, setValues] = React.useState({
+  const [values, setValues] = useState({
     amount: '',
     password: '',
     weight: '',
     weightRange: '',
     showPassword: false,
   });
+  const [checked, setChecked] = useState(false);
+
+  const handleCheckBox = (event) => {
+    setChecked(event.target.checked);
+  };
 
   const handleChange = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
@@ -47,65 +54,13 @@ const GuestForm = () => {
     event.preventDefault();
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  // };
 
   return (
     <FormWrapper onSubmit={handleSubmit}>
-      <FormControl sx={{ m: 1, width: '25ch' }} variant='standard'>
-        <TextField
-          // error
-          id='standard-textarea'
-          label='First Name'
-          placeholder='John'
-          multiline
-          variant='standard'
-          helperText='Incorrect entry.'
-          required
-        />
-      </FormControl>
-      <FormControl sx={{ m: 1, width: '25ch' }} variant='standard'>
-        <TextField
-          id='standard-textarea'
-          label='Last Name'
-          placeholder='Doe'
-          multiline
-          variant='standard'
-        />
-      </FormControl>
-      <FormControl sx={{ m: 1, width: '25ch' }} variant='standard'>
-        <TextField
-          id='standard-textarea'
-          label='E-mail Address'
-          placeholder='example@example.com'
-          multiline
-          variant='standard'
-        />
-      </FormControl>
-      <FormControl sx={{ m: 1, width: '25ch' }} variant='standard'>
-        <InputLabel htmlFor='standard-adornment-password'>Password</InputLabel>
-        <Input
-          id='standard-adornment-password'
-          type={values.showPassword ? 'text' : 'password'}
-          value={values.password}
-          onChange={handleChange('password')}
-          endAdornment={
-            <InputAdornment position='end'>
-              <IconButton
-                aria-label='toggle password visibility'
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-              >
-                {values.showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          }
-        />
-      </FormControl>
-      <button type='submit'>submit</button>
-
-      {/* <InputWrapper
+      <InputWrapper
         type='text'
         placeholder='First name...'
         {...register('firstName', {
@@ -116,30 +71,76 @@ const GuestForm = () => {
         }
       />
       {errors.firstName && <PWrapper>{errors.firstName.message}</PWrapper>}
+      {/* <FormControl sx={{ m: 1, width: '25ch' }} variant='standard'>
+        <TextField
+          // error
+          id='standard-textarea'
+          label='First Name'
+          placeholder='John'
+          multiline
+          variant='standard'
+          helperText='Incorrect entry.'
+        />
+      </FormControl> */}
+      <FormControl sx={{ m: 1, width: '25ch' }} variant='standard'>
+        <TextField
+          id='standard-textarea'
+          label='Last Name'
+          placeholder='Doe'
+          multiline
+          variant='standard'
+          {...register('lastName', { pattern: /^[A-Za-z]+$/i, required: true })}
+        />
+        {errors?.lastName?.type === 'pattern' && (
+          <p>Alphabetical characters only</p>
+        )}
+      </FormControl>
 
-      <InputWrapper
-        type='text'
-        placeholder='Last name...'
-        {...register('lastName', {
-          required: 'Please enter your last name!',
-        })}
-        onChange={(e) =>
-          setGuestUserOrder({ ...guestUserOrder, lastName: e.target.value })
+      <FormControl sx={{ m: 1, width: '25ch' }} variant='standard'>
+        <TextField
+          id='standard-textarea'
+          label='E-mail Address'
+          placeholder='example@example.com'
+          multiline
+          variant='standard'
+        />
+      </FormControl>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={checked}
+            onChange={handleCheckBox}
+            inputProps={{ 'aria-label': 'controlled' }}
+          />
         }
+        label='Create an Account'
       />
-      {errors.lastName && <PWrapper>{errors.lastName.message}</PWrapper>}
+      {checked && (
+        <FormControl sx={{ m: 1, width: '25ch' }} variant='standard'>
+          <InputLabel htmlFor='standard-adornment-password'>
+            Password
+          </InputLabel>
+          <Input
+            id='standard-adornment-password'
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position='end'>
+                <IconButton
+                  aria-label='toggle password visibility'
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      )}
 
-      <InputWrapper
-        type='text'
-        placeholder='E-mail address...'
-        {...register('email', {
-          required: 'please enter your e-mail address!',
-        })}
-        onChange={(e) =>
-          setGuestUserOrder({ ...guestUserOrder, email: e.target.value })
-        }
-      />
-      {errors.email && <PWrapper>{errors.email.message}</PWrapper>} */}
+      <button type='submit'>submit</button>
     </FormWrapper>
   );
 };
