dc2664a43386e7a17336d48ab14ef5d6e4affdbe
Hani al Sabti <h.sabti87@gmail.com>
2021-10-14 17:24:12 +0200

    created update account skeleton

--- client/src/components/user/pages/components/manageAccount/accountStyles.js
+++ client/src/components/user/pages/components/manageAccount/accountStyles.js
@@ -2,6 +2,13 @@ import { makeStyles } from '@mui/styles/';
 
 export const useStyles = makeStyles((theme, prop) => {
   return {
+    box: {
+      '&&': {
+        display: 'flex',
+        position: 'relative',
+        maxWidth: '1190px',
+      },
+    },
     appBar: {
       '&&': {
         marginTop: '82px',
@@ -13,9 +20,52 @@ export const useStyles = makeStyles((theme, prop) => {
     drawer: {
       '&&': {
         '& .MuiDrawer-paper': {
-          top: '82px',
+          position: 'absolute',
           zIndex: 0,
         },
+      },
+    },
+    listItem: {
+      '&&': {
+        [theme.breakpoints.down('sm')]: {
+          padding: '8px 16px',
+        },
+      },
+    },
+    listIcon: {
+      '&&': {
+        color: 'rgba(0, 0, 0, 0.83)',
+      },
+    },
+    bottomToolbar: {
+      '&&': {
+        maxWidth: '500px',
+        margin: '0 auto',
+        justifyContent: 'center',
+        [theme.breakpoints.down('sm')]: {
+          marginLeft: '50px',
+        },
+      },
+    },
+    accountTitle: {
+      '&&': {
+        [theme.breakpoints.down('md')]: {
+          fontSize: '28px',
+        },
+        [theme.breakpoints.between('xs', 'xss')]: {
+          fontSize: '24px',
+        },
+      },
+    },
+    mainContainer: {
+      '&&': {
+        // [theme.breakpoints.down('md')]: {
+        //   fontSize: '28px',
+        // },
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '200px',
       },
     },
   };
---
+++ client/src/components/user/pages/components/updateAccount/UpdateAccount.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UpdateAccount = () => {
+  return <div></div>;
+};
+
+export default UpdateAccount;
