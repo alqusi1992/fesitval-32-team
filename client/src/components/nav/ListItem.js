8f4cc9ecc300acd2cfda0f649fa33b13a0ba16a2
Ameri <mohammedagl6@gmail.com>
2021-10-16 14:26:01 +0200

    added user menu style

--- client/src/components/nav/ListItem.js
+++ client/src/components/nav/ListItem.js
@@ -2,11 +2,13 @@ import { Button, Grid } from '@mui/material';
 import React from 'react';
 import { useStyles } from './NavStyles';
 
-export const ListItem = ({ text, drawer }) => {
+export const ListItem = ({ text, drawer, handleClick }) => {
   const classes = useStyles({ drawer });
   return (
     <Grid item alignItems='center'>
-      <Button className={classes.listItemBtn}>{text}</Button>
+      <Button className={classes.listItemBtn} onClick={handleClick}>
+        {text}
+      </Button>
     </Grid>
   );
 };
--- client/src/components/nav/NavUser/NavUser.js
+++ client/src/components/nav/NavUser/NavUser.js
@@ -1,12 +1,20 @@
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import { IconButton, Menu, MenuItem, Button } from '@mui/material/';
+import {
+  IconButton,
+  Menu,
+  MenuItem,
+  Button,
+  Avatar,
+  ListItemIcon,
+  Divider,
+  Tooltip,
+} from '@mui/material/';
 import { useState } from 'react';
-import LogoutIcon from '@mui/icons-material/Logout';
+import Logout from '@mui/icons-material/Logout';
 import LockIcon from '@mui/icons-material/Lock';
 import { useValue } from '../../../context/globalContext';
 import { logout, setUser } from '../../../actions/userActions';
 import User from '../../user/User';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { useEffect } from 'react';
 import { getLocalStorage } from '../../../utils/localStorage';
 import decode from 'jwt-decode';
@@ -16,6 +24,7 @@ const NavUser = ({ drawer }) => {
   const history = useHistory();
   const classes = useStyles({ drawer });
   const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
   const [isRegister, setIsRegister] = useState(false);
   const {
     state: { user },
@@ -44,7 +53,6 @@ const NavUser = ({ drawer }) => {
   }
 
   const handleLogout = () => {
-    handleClose();
     logout(dispatch);
     history.push('/');
   };
@@ -69,36 +77,57 @@ const NavUser = ({ drawer }) => {
 
   return (
     <div>
-      <IconButton
-        size='large'
-        aria-label='account of current user'
-        aria-controls='menu-appbar'
-        aria-haspopup='true'
-        onClick={handleMenu}
-      >
-        <AccountCircle style={{ fontSize: '35px' }} />
-      </IconButton>
+      <Tooltip title='Account settings'>
+        <IconButton onClick={handleMenu} size='large' sx={{ ml: 2 }}>
+          <Avatar sx={{ width: 32, height: 32 }}>
+            {user?.result?.firstName?.charAt(0)?.toUpperCase() ||
+              user?.result?.givenName?.charAt(0)?.toUpperCase()}
+          </Avatar>
+        </IconButton>
+      </Tooltip>
       <Menu
-        id='menu-appbar'
         anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        keepMounted
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        open={Boolean(anchorEl)}
+        open={open}
         onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
-        <Link to='/account' style={{ color: '#000' }}>
-          <MenuItem onClick={handleClose}>My account</MenuItem>
-        </Link>
+        <MenuItem onClick={() => history.push('/account')}>
+          <Avatar /> My account
+        </MenuItem>
+        <Divider />
         <MenuItem onClick={handleLogout}>
-          <LogoutIcon />
-          <span style={{ marginLeft: '5px' }}>Logout</span>
+          <ListItemIcon>
+            <Logout fontSize='small' />
+          </ListItemIcon>
+          Logout
         </MenuItem>
       </Menu>
     </div>
