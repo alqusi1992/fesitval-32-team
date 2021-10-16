98bcf06fd17e3679b57e3b44bf0d8c9cc27999f9
Mohammad <malnajjar16@gmail.com>
2021-10-16 10:43:36 +0200

    register validation fase 2

--- client/src/components/user/register/Register.js
+++ client/src/components/user/register/Register.js
@@ -43,8 +43,8 @@ const Register = ({ setIsRegister }) => {
   } = useForm();
 
   const handleChange = (e) => {
-    console.log(e.target.name);
-    setUserData({ ...userData, [e.target.name]: e.target.value });
+    console.log(e.target.value);
+    setUserData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
   const handleClickShowPassword = () => {
     setShowPassword({
@@ -64,7 +64,7 @@ const Register = ({ setIsRegister }) => {
   } = useValue();
 
   const registerHandler = async (e) => {
-    console.log(userData);
+    console.log(e);
     if (userData.password === userData.confirmPassword) {
       const response = await register(userData, dispatch);
       if (response.success) {
@@ -90,12 +90,12 @@ const Register = ({ setIsRegister }) => {
               placeholder='John'
               multiline
               variant='standard'
-              defaultValue={userData.firstName}
-              onChange={handleChange}
+              // value={userData.firstName}
               error={errors?.firstName?.type ? true : false}
               helperText={errors?.firstName?.message}
               {...registerForm('firstName', {
                 required: 'Please insert your first name!',
+                // onChange: handleChange,
                 pattern: {
                   value: /^[A-Za-z]+$/i,
                   message: 'Insert only letters',
@@ -111,8 +111,8 @@ const Register = ({ setIsRegister }) => {
               type='text'
               name='lastName'
               placeholder='Doe'
-              defaultValue={userData.lastName}
-              onChange={handleChange}
+              // defaultValue={userData.lastName}
+              // onChange={handleChange}
               helperText={errors?.lastName?.message}
               {...registerForm('lastName', {
                 required: 'Please insert your last name!',
@@ -130,8 +130,8 @@ const Register = ({ setIsRegister }) => {
               variant='standard'
               name='email'
               placeholder='email@example.com'
-              defaultValue={userData.email}
-              onChange={handleChange}
+              // defaultValue={userData.email}
+              // onChange={handleChange}
               helperText={errors?.email?.message}
               {...registerForm('email', {
                 required: 'Please insert your email!',
@@ -151,8 +151,8 @@ const Register = ({ setIsRegister }) => {
               helperText={errors?.phone?.message}
               type='tel'
               name='phone'
-              defaultValue={userData.phone}
-              onChange={handleChange}
+              // defaultValue={userData.phone}
+              // onChange={handleChange}
               {...registerForm('phone', {
                 pattern: {
                   value:
@@ -174,8 +174,8 @@ const Register = ({ setIsRegister }) => {
                 variant='standard'
                 helperText={errors?.password?.message}
                 name='password'
-                defaultValue={userData.password}
-                onChange={handleChange}
+                // defaultValue={userData.password}
+                // onChange={handleChange}
                 type={showPassword.newPassword ? 'text' : 'password'}
                 {...registerForm('password', {
                   required: 'Please insert a password!',
@@ -209,16 +209,16 @@ const Register = ({ setIsRegister }) => {
               variant='standard'
             >
               <TextField
-                error={errors?.password?.type ? true : false}
+                error={errors?.confirmPassword?.type ? true : false}
                 label='Confirm password'
                 placeholder='Confirm Password'
                 variant='standard'
-                helperText={errors?.password?.message}
+                helperText={errors?.confirmPassword?.message}
                 name='confirmPassword'
-                defaultValue={userData.confirmPassword}
-                onChange={handleChange}
+                // defaultValue={userData.confirmPassword}
+                // onChange={handleChange}
                 type={showPassword.confirmPassword ? 'text' : 'password'}
-                {...registerForm('password', {
+                {...registerForm('confirmPassword', {
                   required: 'Please insert a password!',
                   pattern: {
                     value:
@@ -254,108 +254,3 @@ const Register = ({ setIsRegister }) => {
 };
 
 export default Register;
-
-/*
- <TextField
-        error={errors?.firstName?.type ? true : false}
-        label='FirstName'
-        variant='standard'
-        onChange={(e) => setData({ ...data, firstName: e.target.value })}
-        helperText={errors?.firstName?.message}
-        {...register('firstName', {
-          required: 'Please insert your first name!',
-          pattern: {
-            value: /^[A-Za-z]+$/i,
-            message: 'Insert only letters',
-          },
-        })}
-      />
-      <TextField
-        error={errors?.lastName?.type ? true : false}
-        label='LastName'
-        variant='standard'
-        onChange={(e) => setData({ ...data, lastName: e.target.value })}
-        helperText={errors?.lastName?.message}
-        {...register('lastName', {
-          required: 'Please insert your last name!',
-          pattern: {
-            value: /^[A-Za-z]+$/i,
-            message: 'Insert only letters',
-          },
-        })}
-      />
-      <TextField
-        error={errors?.email?.type ? true : false}
-        label='Emial'
-        variant='standard'
-        onChange={(e) => setData({ ...data, email: e.target.value })}
-        helperText={errors?.email?.message}
-        {...register('email', {
-          required: 'Please insert your email!',
-          pattern: {
-            value:
-              /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            message: 'Insert a valid email!',
-          },
-        })}
-      />
-      <TextField
-        error={errors?.phone?.type ? true : false}
-        label='Phone'
-        variant='standard'
-        helperText={errors?.phone?.message}
-        onChange={(e) => setData({ ...data, phone: e.target.value })}
-        {...register('phone', {
-          pattern: {
-            value: /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/im,
-            message: 'Insert Valid Phone Number',
-          },
-        })}
-      />
-      <FormControl sx={{ m: 1, width: '25ch', position: 'relative' }} variant='standard'>
-        <TextField
-          label='CurrentPassword'
-          variant='standard'
-          onChange={(e) => setData({ ...data, currentPassword: e.target.value })}
-          type={data.showCurrentPassword ? 'text' : 'password'}
-        />
-        <InputAdornment sx={{ position: 'absolute', right: '0', top: '30px' }}>
-          <IconButton aria-label='toggle password visibility' onClick={handleClickCurrentPassword}>
-            {data.showCurrentPassword ? <VisibilityOff /> : <Visibility />}
-          </IconButton>
-        </InputAdornment>
-      </FormControl>
-
-      <FormControl sx={{ m: 1, width: '25ch', position: 'relative' }} variant='standard'>
-        <TextField
-          error={errors?.password?.type ? true : false}
-          label='NewPassword'
-          variant='standard'
-          onChange={(e) => setData({ ...data, newPassword: e.target.value })}
-          type={data.showNewPassword ? 'text' : 'password'}
-          helperText={errors?.password?.message}
-          {...register('password', {
-            required: 'Please insert a password!',
-            pattern: {
-              value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
-              message:
-                'Password should have at least 8 characters, one uppercase, one lowercase,one number and one special character!',
-            },
-          })}
-        />
-        <InputAdornment sx={{ position: 'absolute', right: '0', top: '30px' }}>
-          <IconButton aria-label='toggle password visibility' onClick={handleClickNewPassword}>
-            {data.showNewPassword ? <VisibilityOff /> : <Visibility />}
-          </IconButton>
-        </InputAdornment>
-      </FormControl>
-
-      <Button
-        className={classes.buttonUpdate}
-        variant='contained'
-        onClick={handleSubmit(fetchUser)}
-        sx={{ display: 'block', marginLeft: '100px' }}
-      >
-        SUBMIT
-      </Button>
-*/
