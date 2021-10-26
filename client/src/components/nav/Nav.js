158dc9fc31ae67819ba09724d7f2d9b02af5f049
Hani al Sabti <h.sabti87@gmail.com>
2021-10-26 11:47:14 +0200

    improved variable naming

--- client/src/components/nav/Nav.js
+++ client/src/components/nav/Nav.js
@@ -3,16 +3,19 @@ import { useMediaQuery } from '@mui/material';
 import { NavBar } from './NavBar';
 
 const Nav = () => {
-  const matches = useMediaQuery('(min-width:832px)');
+  const isMatch = useMediaQuery('(min-width:832px)');
   const [drawer, setDrawer] = useState(false);
   const handleDrawer = () => {
     setDrawer(!drawer);
   };
 
   return (
-    <>
-      <NavBar handleDrawer={handleDrawer} matches={matches} drawer={drawer} />
-    </>
+    <NavBar
+      handleDrawer={handleDrawer}
+      isMatch={isMatch}
+      drawer={drawer}
+      setDrawer={setDrawer}
+    />
   );
 };
 
