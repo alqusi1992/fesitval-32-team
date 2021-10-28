d87fb2b1076b8505047d7dd6918663fa1b3b0e17
“Mohammad <malqusi@yahoo.com>
2021-10-28 13:59:29 +0200

    styling order summary desktop version

--- client/src/components/orderStepper/orderSummary/OrderSummary.jsx
+++ client/src/components/orderStepper/orderSummary/OrderSummary.jsx
@@ -2,6 +2,7 @@ import { Grid } from '@mui/material';
 import React from 'react';
 import { PayButton } from '../..';
 import { useGuestContext } from '../../../context/guestContext';
+import { useStyles } from '../orderSummary/OrderSummaryStyles';
 
 const OrderSummary = () => {
   const { guestUserOrder } = useGuestContext();
@@ -9,6 +10,7 @@ const OrderSummary = () => {
     userInfo: { firstName, lastName, email },
     tickets,
   } = guestUserOrder;
+  const classes = useStyles();
 
   const totalTicketsPrice = guestUserOrder.tickets.reduce(
     (acc, value) => acc + value.price * value.quantity,
@@ -16,51 +18,65 @@ const OrderSummary = () => {
   );
 
   return (
-    <Grid container>
-      <Grid container justifyContent='center' item xs={12}>
-        <h2>Buy Your Tickets</h2>
-      </Grid>
-      <Grid container item xs={12}>
-        <Grid item xs={12}>
-          First name: {firstName}
+    <Grid container position='relative'>
+      <Grid container className={classes.tableOrder}>
+        <Grid container justifyContent='center' item xs={12}>
+          <div className={classes.behindColor}>
+            <h2 className={classes.BuyTicketTitle} justifyContent='center'>
+              ORDER OVERVIEW
+            </h2>
+          </div>
         </Grid>
-        <Grid item xs={12}>
-          Last name: {lastName}
-        </Grid>
-        <Grid item xs={12}>
-          Email address: {email}
-        </Grid>
-      </Grid>
-      <Grid container item xs={12}>
-        <Grid container item xs={12}>
-          <Grid item xs={4}>
-            Ticket Type
+        <div className={classes.userContainer}>
+          <Grid container item xs={12} className={classes.userInfo}>
+            <div>
+              <h3 className={classes.personalDetailsHeader}>PERSONAL DETAILS</h3>
+              <div className={classes.personalDetails}>
+                <Grid item xs={12}>
+                  First name: {firstName}
+                </Grid>
+                <Grid item xs={12}>
+                  Last name: {lastName}
+                </Grid>
+                <Grid item xs={12}>
+                  Email address: {email}
+                </Grid>
+              </div>
+            </div>
           </Grid>
-          <Grid item xs={4}>
-            Price/ticket
-          </Grid>
-          <Grid item xs={4}>
-            Quantity
-          </Grid>
-        </Grid>
-        {tickets.map((ticket, idx) => (
-          <Grid container item xs={12} key={idx}>
-            <Grid item xs={4}>
-              {ticket.typeName}
+          <Grid container item xs={12} className={classes.orderContainer}>
+            <Grid container item xs={12} className={classes.orderTitels}>
+              <Grid item xs={4}>
+                Ticket Type
+              </Grid>
+              <Grid item xs={4}>
+                Price/ticket
+              </Grid>
+              <Grid item xs={4}>
+                Quantity
+              </Grid>
             </Grid>
-            <Grid item xs={4}>
-              € {ticket.price}
-            </Grid>
-            <Grid item xs={4}>
-              {ticket.quantity}
+
+            {tickets.map((ticket, idx) => (
+              <Grid container item xs={12} key={idx} className={classes.orderInfo}>
+                <Grid item xs={4}>
+                  {ticket.typeName}
+                </Grid>
+                <Grid item xs={4}>
+                  € {ticket.price}
+                </Grid>
+                <Grid item xs={4}>
+                  {ticket.quantity}
+                </Grid>
+              </Grid>
+            ))}
+            <Grid container item xs={12} className={classes.totalPrice}>
+              <Grid textAlign='center'>Total: € {totalTicketsPrice}</Grid>
             </Grid>
           </Grid>
-        ))}
+        </div>
       </Grid>
-      <Grid container item xs={12}>
-        <Grid>Total: € {totalTicketsPrice}</Grid>
-      </Grid>
-      <Grid container item xs={12}>
+      <Grid container item xs={12} className={classes.buttonPay}>
         <PayButton />
       </Grid>
     </Grid>
---
+++ client/src/components/orderStepper/orderSummary/OrderSummaryStyles.js
@@ -0,0 +1,69 @@
+import { makeStyles } from '@mui/styles/';
+
+export const useStyles = makeStyles((theme, prop) => {
+  return {
+    tableOrder: {
+      border: '3px solid #581127',
+      marginTop: '100px',
+    },
+    behindColor: {
+      backgroundColor: '#581127',
+      width: '115vh',
+    },
+    BuyTicketTitle: {
+      color: '#fff',
+      textAlign: 'center',
+    },
+    userContainer: {
+      display: 'flex',
+      margin: '0 auto',
+      padding: '15px',
+    },
+
+    userInfo: {
+      border: '2px solid #DDD',
+      flex: '6 !important',
+      height: '300px',
+      margin: '30px 5px !important',
+    },
+    personalDetails: {
+      padding: '20px 20px',
+    },
+
+    orderContainer: {
+      border: '2px solid #DDD',
+      margin: '30px 5px !important',
+      flex: '10 !important',
+    },
+
+    personalDetailsHeader: {
+      backgroundColor: '#DDD',
+      padding: '15px',
+      margin: '0',
+      width: '24vw',
+    },
+    orderTitels: {
+      backgroundColor: '#DDD',
+      padding: '15px',
+      fontWeight: 'bold',
+      fontSize: '1.2em',
+      height: '58px',
+    },
+    orderInfo: {
+      padding: '15px',
+    },
+    totalPrice: {
+      borderTop: '2px solid #DDD',
+      padding: '10px',
+      fontWeight: 'bold',
+      display: 'flex',
+      justifyContent: 'flex-end',
+    },
+
+    buttonPay: {
+      position: 'absolute',
+      bottom: '-50px',
+      right: '-930px',
+    },
+  };
+});
