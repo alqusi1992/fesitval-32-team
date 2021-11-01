b29e9716defb3ca79007fbe13e06eb0580eb4370
“Mohammad <malqusi@yahoo.com>
2021-11-01 15:53:38 +0100

    finished styeling order account

---
+++ client/src/components/user/pages/components/OrderStyle.js
@@ -0,0 +1,52 @@
+export const classes = {
+  tableOrder: {
+    border: '3px solid #581127',
+    marginTop: '1.5vh',
+    minWidth: '60vw',
+  },
+  orderHeader: {
+    backgroundColor: '#581127',
+    padding: '1.2rem',
+    width: '100%',
+    fontSize: '1rem',
+    color: '#fff',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    fontWeight: 'bold',
+    alignItems: 'center',
+  },
+  orderTitles: {
+    backgroundColor: '#DDD',
+    width: '100%',
+    padding: '0.8rem',
+    margin: '0',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  orderContainer: {
+    width: '95%',
+    margin: '1rem auto',
+    border: '2px solid #DDD',
+  },
+  orderDetails: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '100%',
+    padding: '0.8rem',
+  },
+
+  orderItem: {
+    width: '5rem',
+    textAlign: 'center',
+  },
+  totalPrice: {
+    borderTop: '2px solid #DDD',
+    padding: '0.3rem',
+    fontWeight: 'bold',
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '100%',
+    fontSize: '1.1rem',
+  },
+};
