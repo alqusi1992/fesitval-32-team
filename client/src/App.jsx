f6d2bc29769655137eb91ef44d53d7543f8d1925
Yahya Ganjo <yahya.ganjo@gmail.com>
2021-11-02 13:44:58 +0100

    using sx prop instead of inline style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import { ThemeProvider } from '@mui/material/styles';
-import { theme } from './GlobalStyles';
+import { theme, classes } from './GlobalStyles';
 import {
   LandingPage,
   SuccessPage,
@@ -29,7 +29,7 @@ function App() {
         <NavBar />
         <Loading />
         <Verify />
-        <Container maxWidth='lg' sx={{ padding: '50px 0 350px' }}>
+        <Container maxWidth='lg' sx={classes.container}>
           <Switch>
             <Route exact path='/' component={LandingPage} />
             <Route exact path='/success' component={SuccessPage} />
