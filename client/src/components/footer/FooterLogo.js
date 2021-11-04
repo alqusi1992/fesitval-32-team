37c4efcaa904d819da98d71336f5fd77dafb57bf
Mouawya <mouawya88@gmail.com>
2021-11-04 14:42:12 +0100

    changed footer styling type

--- client/src/components/footer/FooterLogo.js
+++ client/src/components/footer/FooterLogo.js
@@ -1,11 +1,12 @@
 import { Grid } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { classes } from './FooterStyles';
 
 const FooterLogo = () => {
   return (
     <Link to='/'>
-      <Grid sx={{ fontSize: '50px', color: '#fff' }}>Festival32</Grid>
+      <Grid sx={classes.logo}>Festival32</Grid>
     </Link>
   );
 };
