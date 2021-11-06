6f33bc73423cc88c9ea1f51d1901f013cbb22625
Mohammad <malnajjar16@gmail.com>
2021-11-07 00:02:16 +0100

    fix link for program page

--- client/src/components/pages/landingPage/landingPage.js
+++ client/src/components/pages/landingPage/landingPage.js
@@ -50,7 +50,7 @@ const LandingPage = () => {
       showAlert(
         'danger',
         'Link is expired, please request a new link',
-        dispatch
+        dispatch,
       );
     } else {
       showAlert('danger', 'Verification is failed', dispatch);
@@ -73,7 +73,7 @@ const LandingPage = () => {
           </Link>
         </ListItem>
         <ListItem>
-          <Link to='/schedule'>
+          <Link to='/program'>
             <ImageCard
               {...{ img: program, title: 'PROGRAM', buttonTitle: 'PROGRAM' }}
             />
